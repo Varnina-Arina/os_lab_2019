c6456c6694560c250fe7ddeb8cade7d3333c64a4
Varnina-Arina <arinavarnina@yandex.ru>
2025-04-17 17:33:36 +0000

    Выполнена лабораторная работа №2

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,19 @@
 #include "revert_string.h"
+#include "string.h"
+
+void Swap(char *left, char *right)
+{
+	char tmp = *left;
+	*left = *right;
+	*right = tmp;
+}
 
 void RevertString(char *str)
 {
-	// your code here
+	int n = strlen(str);
+	for (int i = 0; i < n / 2; ++i)
+	    Swap(&str[i], &str[n-i-1]);
 }
+
+
 
